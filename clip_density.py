c9d5934c840b997ba8861c9a1135bc54e6dec408
PrestonHill <32971302+PrestonHill@users.noreply.github.com>
2017-11-15 20:47:27 -0600

    Update clip_density.py

--- clip_density.py
+++ clip_density.py
@@ -5,7 +5,7 @@ def clip1():
     
     arcpy.Clip_management()
 
-def density():
+def density(): #FIRST CODE USED
 
     my_items = collections.defaultdict(set)
     for row in cur1:
@@ -21,6 +21,20 @@ def density():
         dicts[n] = values[i / total]
         n += 1
     print(dicts)
+    
+#SECOND CODE USED
         
+def density(inras, ratio, classification): #added the needed inputs
+	fc = input1 #Determines file path from user input
+	field = User_Field_Count #Determines pixel count field from user input
+	cursor = arcpy.SearchCursor(fc)
+	for row in cursor: #Calculates sum of all pixel counts
+    		total = sum(row.getValue(field)) 
 
+	final_ratio = float(classification)/float(total) #Calculates ratio for user input classification
+	
+	if final_ratio >= ratio: 
+		return True, final_ratio #Returns true and final ratio if user input is met
+	else:
+		return False, final_ratio #Returns false and final ratio if user input is not met
     
